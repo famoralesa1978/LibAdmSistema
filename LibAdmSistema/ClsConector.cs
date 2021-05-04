ddaed5c0a471c99e92ec3811c4d5abc893ca8e34
felipe morales <fmorales_arenas@hotmail.com>
2021-05-04 01:04:53 -0400

    Agregar archivos de proyecto.

---
+++ LibAdmSistema/ClsConector.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Data.Sql;
+
+namespace LibAdmSistema
+{
+	public class ClsConector
+	{
+		private SqlConnection conexion;
+		private LibAdmSistema.ClsUtilidades clsUtiles = new LibAdmSistema.ClsUtilidades();
+		//private SqlConnection conexion;
+
+		private void Conectar(String strPublicacion)
+		{
+
+			string bdProduccion = "Pn6QdbLxN6zYhNuC0AGO9QzP8WL2RI9VHfd/l56YcLkZ1UdzuJNuXq3s7y9ZY3eq6QrxfamnP0GH0FDdEHA6bAWJdHonailm8a5b3eyUw5vuWLyX+mBmFPxKLHFVjRtYm0sjwb1KdqM=";//original 
+			string bdDesarrollo = "qbz4h7qjqnqSjENx4B27ugzP8WL2RI9VHfd/l56YcLkmIh9Uvox9JOk1DSagQtDL4h4EPWVkA543Pays15jQmJINFUIAe7aRhB8CSIm5+laiVatmvADwWL5dJ6Rx6eYZnZ6GlbhcqWa6vQJjhPxaQQ==";//original 
+
+			string bd = strPublicacion == "Desarrollo" ? bdDesarrollo : strPublicacion == "Prod1" ? bdProduccion : "";
+
+			//**********************
+			conexion = new SqlConnection(clsUtiles.DecryptTripleDES(bd));
+
+			conexion.Open();
+
+		}
+
+		private void Cerrar()
+		{
+
+			conexion.Close();
+		}
+
+		public DataSet Listar(String strPublicacion, SqlCommand cmd)
+		{
+			Conectar(strPublicacion);
+			DataSet dt = new DataSet();
+			cmd.Connection = conexion;
+			SqlDataAdapter reader = new SqlDataAdapter(cmd);
+			reader.Fill(dt);
+			Cerrar();
+			return dt;
+		}
+
+		public void AgregarModificarEliminar(String strPublicacion, SqlCommand cmd)
+		{
+			Conectar(strPublicacion);
+
+			cmd.Connection = conexion;
+
+			cmd.ExecuteNonQuery();
+
+
+			Cerrar();
+
+		}
+
+	}
+}
---
+++ LibAdmSistema/ClsUtilidades.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+
+
+namespace LibAdmSistema
+{
+	class ClsUtilidades
+	{
+
+    private SymmetricAlgorithm alg;
+    const string sKey = "compass";
+
+    #region "Contraseñas"
+
+    public string GenerateHashMD5(string input)
+    {
+      TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+      MD5CryptoServiceProvider hashMD5 = new MD5CryptoServiceProvider();
+      //  Compute the MD5 hash.
+      DES.Key = hashMD5.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(sKey));
+      //  Set the cipher mode.
+      DES.Mode = CipherMode.ECB;
+      //  Create the encryptor.
+      ICryptoTransform DESEncrypt = DES.CreateEncryptor();
+      //  Get a byte array of the string.
+      byte[] Buffer = System.Text.ASCIIEncoding.ASCII.GetBytes(input);
+      //  Transform and return the string.
+      return Convert.ToBase64String(DESEncrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+    }
+
+    public string DecryptTripleDES(string sOut)
+    {
+      TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+      MD5CryptoServiceProvider hashMD5 = new MD5CryptoServiceProvider();
+      //  Compute the MD5 hash.
+      DES.Key = hashMD5.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(sKey));
+      //  Set the cipher mode.
+      DES.Mode = CipherMode.ECB;
+      //  Create the decryptor.
+      ICryptoTransform DESDecrypt = DES.CreateDecryptor();
+      byte[] Buffer = Convert.FromBase64String(sOut);
+      //  Transform and return the string.
+      return System.Text.ASCIIEncoding.ASCII.GetString(DESDecrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+    }
+
+    public Byte[] insertarPDF(string pdfnombre)
+    {
+      FileStream fs = new FileStream(pdfnombre, FileMode.Open, FileAccess.Read);
+      BinaryReader br = new BinaryReader(fs);
+      Byte[] bytes = br.ReadBytes((Int32)fs.Length);
+      br.Close();
+      fs.Close();
+      return bytes;
+    }
+
+    #endregion
+
+  }
+}
