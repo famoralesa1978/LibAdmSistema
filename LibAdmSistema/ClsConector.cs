ebd04f7d47ec88bc3a6c7c06df73854ef8764cdb
Camilo Valle Calderon <camilo.valle@ine.cl>
2021-05-17 20:57:58 -0400

    - Incorporación de conexión a bd de administración.

--- LibAdmSistema/ClsConector.cs
+++ LibAdmSistema/ClsConector.cs
@@ -14,30 +14,22 @@ namespace LibAdmSistema
 		private LibAdmSistema.ClsUtilidades clsUtiles = new LibAdmSistema.ClsUtilidades();
 		//private SqlConnection conexion;
 
-		private void Conectar(String strPublicacion)
+		private void Conectar()
 		{
-
-			string bdProduccion = "Pn6QdbLxN6zYhNuC0AGO9QzP8WL2RI9VHfd/l56YcLkZ1UdzuJNuXq3s7y9ZY3eq6QrxfamnP0GH0FDdEHA6bAWJdHonailm8a5b3eyUw5vuWLyX+mBmFPxKLHFVjRtYm0sjwb1KdqM=";//original 
-			string bdDesarrollo = "qbz4h7qjqnqSjENx4B27ugzP8WL2RI9VHfd/l56YcLkmIh9Uvox9JOk1DSagQtDL4h4EPWVkA543Pays15jQmJINFUIAe7aRhB8CSIm5+laiVatmvADwWL5dJ6Rx6eYZnZ6GlbhcqWa6vQJjhPxaQQ==";//original 
-
-			string bd = strPublicacion == "Desarrollo" ? bdDesarrollo : strPublicacion == "Prod1" ? bdProduccion : "";
-
-			//**********************
-			conexion = new SqlConnection(clsUtiles.DecryptTripleDES(bd));
-
+			string bd = "qbz4h7qjqnqSjENx4B27ugzP8WL2RI9VHfd/l56YcLkmIh9Uvox9JOk1DSagQtDL4h4EPWVkA543Pays15jQmJINFUIAe7aRhB8CSIm5+laiVatmvADwWL5dJ6Rx6eYZnZ6GlbhcqWa6vQJjhPxaQQ==";
+            //**********************
+            conexion = new SqlConnection(clsUtiles.DecryptTripleDES(bd));
 			conexion.Open();
-
 		}
 
 		private void Cerrar()
 		{
-
 			conexion.Close();
 		}
 
 		public DataSet Listar(String strPublicacion, SqlCommand cmd)
 		{
-			Conectar(strPublicacion);
+			Conectar();
 			DataSet dt = new DataSet();
 			cmd.Connection = conexion;
 			SqlDataAdapter reader = new SqlDataAdapter(cmd);
@@ -48,7 +40,7 @@ namespace LibAdmSistema
 
 		public void AgregarModificarEliminar(String strPublicacion, SqlCommand cmd)
 		{
-			Conectar(strPublicacion);
+			Conectar();
 
 			cmd.Connection = conexion;
 
